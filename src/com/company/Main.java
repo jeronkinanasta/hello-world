a42e0a1757809a74b4df195efd9153a3476a91f0
Nastushka Jeronkina <nastushka.jeronkina@gmail.com>
2020-04-17 16:40:55 +0400

    Hello world!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         String hello = "Hello";
-        String world = "World";
+        String world = "world";
         System.out.println(hello + " " + world + "!");
     }
 }
